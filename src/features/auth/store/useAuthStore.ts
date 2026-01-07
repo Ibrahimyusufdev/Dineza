dd854ee0c203b269fb308ffdde317bede867f222
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2026-01-08 00:15:25 +0100

    Wiring up Auth: setting up user, tokens, auth state

--- src/features/auth/store/useAuthStore.ts
+++ src/features/auth/store/useAuthStore.ts
@@ -4,53 +4,115 @@ import { persist } from "zustand/middleware";
 
 import type { UserRole } from "@/types/common";
 
-// Base User Interface
-export interface User {
-  id?: string;
-  name: string;
+// Base User Interface - where I will extend the shape for Diner, Restaurant and admin
+export interface BaseUser {
+  id: string;
   email: string;
+  firstName: string;
+  lastName: string;
   role: UserRole;
+  profilePicture?: string;
+  phoneNumber?: string;
   avatar?: string;
+
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 
-interface AuthState {
-  user: User | null;
-  token?: string | null;
-  error?: string | null;
-  isAuthenticated?: boolean;
-  isLoading?: boolean;
-
-  // Actions
-  setAuth?: (user: User, token: string) => void;
-  logout: () => Promise<void>;
+// Diner User - extending from Base User with specific field added
+export interface Diner extends BaseUser {
+  role: "diner";
+  preferredLocations: string[];
+  totalEarnings?: number;
+  pendingEarnings?: number;
+  availableBalance?: number;
 }
 
-export const useAuthStore = create<AuthState>()(
-  immer(
-    persist(
-      (set) => ({
-        user: { name: "Ibrahim Yusuf", email: "Ibrahimyusuf1304@gmail.com", role: "diner" },
+// Restaurant User - extending from Base User with specific field added
+export interface Restaurant extends BaseUser {
+  role: "restaurant";
+  restaurantName: string;
+  restaurantLogo: string;
+  businessEmail: string;
+  address: string;
+  cuisineType: string;
+  isVerified: boolean;
+  rating?: number;
+  totalReservations?: number;
+  pendingReservations?: number;
+}
 
-        isAuthenticated: true,
-        isLoading: false,
+// Making user to be either Diner or Restaurant using discriminated Union in TS for AuthPurpose
+export type AuthUser = Diner | Restaurant;
 
-        logout: async () => {
-          set((state) => {
-            state.user = null;
-            state.isAuthenticated = false;
-            state.isLoading = false;
-          });
-        },
-      }),
-      {
-        name: "user-storage",
-        partialize: (state) => ({
-          user: state.user,
-          // token: state.token,
-          isAuthenticated: state.isAuthenticated,
-        }),
-      }
-    )
-  )
-);
+// wiring up AuthState
+
+export interface AuthState {
+  // State of data
+  user: AuthUser | null; // Current logged in user or null
+  token: string | null; // JWT token or null
+  error?: string | null; // error from Api
+
+  isAuthenticated: boolean; // Quick boolean check control to tell that a user is authenticated or not
+  isLoading: boolean; // Loading state for Auth check
+  // isCheckingAuth: boolean; // Boolean check if user is authenticated or not
+
+  //Actions
+  // Set Auth - called after successful login/register, and also set User and token, and mark as authenticated
+  setAuth: (user: AuthUser, token: string) => void;
+
+  // Control loading state, and use in auth initialization and Api calls
+  setLoading?: (loading: boolean) => void;
+
+  // Partially update user data
+  updateAuthUser?: (userData: Partial<AuthUser>) => void;
+  
+  // Clears all auth data and reset to initial state 
+  logout: () => void;
+}
+
+/**
+Auth Store - Global Authentication state, persisted to localstorage, so users stay logged in
+ */
+
+export const useAuthStore = create<AuthState>()(immer(persist((set) => ({
+  user: null,
+  token: null,
+  isAuthenticated: false,
+  isLoading: false,
+
+  
+  setAuth: (user, token) => {
+    set((state) => {
+      state.user = user;
+      state.token = token;
+      state.isAuthenticated = true;
+      state.isLoading = false;
+    })
+  },
+
+  setLoading: (loading) => {
+    set((state) => {
+      state.isLoading = loading;
+    })
+  },
+
+  logout: () => {
+    set((state) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+      state.isLoading = false
+    })
+  }
+  
+  
+}), {
+  name: "dineza-auth",
+  partialize: (state) => ({
+    user: state.user,
+    token: state.token,
+    isAuthenticated: state.isAuthenticated
+  })
+})))
